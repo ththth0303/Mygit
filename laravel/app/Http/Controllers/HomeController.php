24df478e6074e879ab83a888714e45b1a8dd4567
ththth0303 <trungthangngo@gmail.com>
2017-04-05 12:48:25 +0700

    develop

--- laravel/app/Http/Controllers/HomeController.php
+++ laravel/app/Http/Controllers/HomeController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     //
-    public function FunctionName($value='')
+    public function FunnName($value='')
     {
     	# code...
     }
